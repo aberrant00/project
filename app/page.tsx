6411741917b3cb036b5ccbd212b1c99650df77e7
aberrant00 <robert.tinca@mail.com>
2023-11-27 20:19:23 +0100

    progress

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
 		<div className="balance">
 			<p className="balance">Balance</p>
 			<p className="march">March 16</p>
-			<p className="total">1.684,33 $</p>
+	<div className="total">		<p className="total">1.684,33 </p><p className="dollar">$</p> </div>
 
   </div>
 <div className="position">
-<div className="income"><p className="balance">Income</p><p className="subtotal">1.322,10 $</p></div>
+<div className="income"><p className="balance">Income</p><p className="subtotal">31.322,10 $</p></div>
 <div className="expense"><p className="balance">Expense</p><p className="subtotal">1.110,25 $</p></div>
 </div>
  </div> 
